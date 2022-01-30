4e0ca26c6a484b01ce0ae0b8daaabb8422958f94
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-01-29 18:15:32 -0800

    display current date update

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 //VARIABLES
 
+    //Date
+    
+
 
 //FUNCTION
 
-//Display Current Day at Top
+    //Function - Display Current Day at Top
+    $("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'));
 
-//Color-code Time Blocks based on Pasat, Present, Future
+    //Color-code Time Blocks based on Pasat, Present, Future
 
-//Click Time Block to Enter Event
+    //Click Time Block to Enter Event
 
-//Click Save Button to Save Time Block (Text is saved in local storage)
+    //Click Save Button to Save Time Block (Text is saved in local storage)
 
-//Refresh Page and Saved Events are Still Displayed 
+    //Refresh Page and Saved Events are Still Displayed 
 
+//ACTIONS
