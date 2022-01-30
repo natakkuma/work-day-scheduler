8f900a1a48451fdc590e714d40a6b66f36a1df03
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-01-29 17:44:05 -0800

    initial fucntions list

---
+++ script.js
@@ -0,0 +1,15 @@
+//VARIABLES
+
+
+//FUNCTION
+
+//Display Current Day at Top
+
+//Color-code Time Blocks based on Pasat, Present, Future
+
+//Click Time Block to Enter Event
+
+//Click Save Button to Save Time Block (Text is saved in local storage)
+
+//Refresh Page and Saved Events are Still Displayed 
+
